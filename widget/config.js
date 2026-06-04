aebeac2aeca16b136525ac233b7a4271add36c13
toph-spot <a.syed@cargobase.com>
2026-06-04 14:17:17 +0800

    Add Metabase configuration in config.js

---
+++ widget/config.js
@@ -0,0 +1,10 @@
+function defineMetabaseConfig(config) {
+  window.metabaseConfig = config;
+}
+defineMetabaseConfig({
+  "theme": {
+    "preset": "light"
+  },
+  "isGuest": true,
+  "instanceUrl": "https://insights.cargobase.com"
+});
